41c14f1bbf5b709bc6298efed6ccbe9edd7b4ee5
Jarek <jaroslaw.wronka@luxmed.pl>
2024-01-31 12:47:51 +0100

    Initial Commit

---
+++ Memories/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memories
+//
+//  Created by Boss on 31/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memories/MemoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriesApp.swift
+//  Memories
+//
+//  Created by Boss on 31/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
