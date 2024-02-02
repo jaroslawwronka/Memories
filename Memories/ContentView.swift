a47fe68d3804baa0900bd8d88c1619d7831c76cf
Jarek <jaroslaw.wronka@luxmed.pl>
2024-02-02 15:22:38 +0100

    Update

--- Memories/ContentView.swift
+++ Memories/ContentView.swift
@@ -9,16 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack{
+            CardView(isFaceUp : false)
+            CardView()
+            CardView(isFaceUp : true)
+            CardView(isFaceUp : true)
+            
         }
+        .foregroundColor(.orange)
         .padding()
+    }
+}
+
+
+struct CardView : View {
+    @State var isFaceUp = false
+    var body: some View {
+        ZStack() {
+            let base = RoundedRectangle(cornerRadius: 12)
+            if isFaceUp {
+                base.fill(.white)
+                base.strokeBorder(lineWidth:2)
+                Text("🙉").font(.largeTitle)
+            } else {
+                base.fill()
+                
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
     }
 }
 
 #Preview {
     ContentView()
 }
+
