194836f64c4b17fb24955b1688a19ca650de3286
Jarek <jaroslaw.wronka@luxmed.pl>
2024-02-05 08:27:41 +0100

    Update

--- Memories/ContentView.swift
+++ Memories/ContentView.swift
@@ -8,23 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis = ["🙉","🐸","🦄","🪽","🪽"]
+    let emojis = ["👻","🎃","🕷️","😈","💀","🕸️","🧙‍♀️","🙀","👹","😱","☠️","🍭"]
+    @State var cardCount : Int = 4
+    
     
     var body: some View {
-        HStack{
-            ForEach(emojis.indices, id: \.self) {
-                index in
-                CardView(content: emojis[index])
-            }
+        VStack{
+            
+            cards
+            cardsCountAdjusters
             
             
             
         }
-        .foregroundColor(.orange)
+        
         .padding()
     }
-}
+    
+    var cards: some View {
+        
+        HStack{
+            ForEach(0..<cardCount, id: \.self) {
+                index in
+                CardView(content: emojis[index])
+            }
+        }
+        .foregroundColor(.orange)
+    }
+    
+    var cardsCountAdjusters: some View {
+        HStack{
+            
+            cardRemover
+            Spacer()
+            cardAdder
+            
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+        
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View{
+        
+        Button(action: {
+           
+                cardCount += offset
+            
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+        
+    }
+    
+    var cardRemover: some View {
+        
+      return cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+        
+    }
+    
+    var cardAdder: some View {
+      return cardCountAdjuster(by: +1, symbol: "rectangle.stack.badge.plus.fill")
 
+    }
+}
 
 struct CardView : View {
     let content: String
