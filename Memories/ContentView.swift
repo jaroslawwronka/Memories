0d7a526e74f6da38d98d9adb8bb49361dfb2e631
Jarek <jaroslaw.wronka@luxmed.pl>
2024-02-05 08:59:13 +0100

    Update

--- Memories/ContentView.swift
+++ Memories/ContentView.swift
@@ -14,8 +14,10 @@ struct ContentView: View {
     
     var body: some View {
         VStack{
-            
-            cards
+            ScrollView {
+                cards
+            }
+            Spacer()
             cardsCountAdjusters
             
             
@@ -27,10 +29,11 @@ struct ContentView: View {
     
     var cards: some View {
         
-        HStack{
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
             ForEach(0..<cardCount, id: \.self) {
                 index in
                 CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .foregroundColor(.orange)
@@ -81,14 +84,13 @@ struct CardView : View {
     var body: some View {
         ZStack() {
             let base = RoundedRectangle(cornerRadius: 12)
-            if isFaceUp {
+            Group {
                 base.fill(.white)
                 base.strokeBorder(lineWidth:2)
                 Text(content).font(.largeTitle)
-            } else {
-                base.fill()
-                
             }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
         }
         .onTapGesture {
             isFaceUp = !isFaceUp
